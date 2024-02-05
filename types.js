d38f754ab4c60f6eb2bd3c6be21ad1c304bf1671
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-02-05 22:18:02 +0530

    add files

---
+++ types.js
@@ -0,0 +1,5 @@
+var myString = "Hello World";
+myString = "Hello World";
+myString = 1;
+var mybool;
+mybool = true;
---
+++ types.ts
@@ -0,0 +1,6 @@
+let myString: string = "Hello World";
+myString = "Hello World";
+myString=1;
+let mybool: boolean;
+
+mybool=true;
